bcb954d847f657164add37c53728ca67d1c1826c
Mark Finger <markfinger@gmail.com>
2014-12-14 18:56:43 +1100

    PyPI tweaks.

--- setup.py
+++ setup.py
@@ -6,14 +6,15 @@ setup(
     name='django-node',
     version=VERSION,
     packages=['django_node'],
+    package_data={
+        'django_node': [
+            'tests/*.py',
+            'tests/package.json',
+        ]
+    },
     install_requires=['django'],
-    description='Django Node',
-    long_description=\
-'''
-Bindings and utils for integrating Node.js and NPM into a Django application.
-
-Documentation at https://github.com/markfinger/django-node
-''',
+    description='Bindings and utils for integrating Node.js and NPM into a Django application',
+    long_description='Documentation at https://github.com/markfinger/django-node',
     author='Mark Finger',
     author_email='markfinger@gmail.com',
     url='https://github.com/markfinger/django-node',
