ff681ce4189cb4e8debb4403135b5460d7b1e4c5
Mark Finger <markfinger@gmail.com>
2015-01-26 19:55:20 +1100

    Bumping the version to 2.2.1 to circumvent PyPI bugs.

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
-VERSION = '2.2.0'
+VERSION = '2.2.1'
 
 setup(
     name='django-node',
     version=VERSION,
     packages=['django_node'],
+    package_data={
+        'django_node': []
+    },
     install_requires=['django'],
     description='Bindings and utils for integrating Node.js and NPM into a Django application',
     long_description='Documentation at https://github.com/markfinger/django-node',
