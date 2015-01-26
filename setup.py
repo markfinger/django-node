8c461960fa500f909cf93eb829f390339e7c437c
Mark Finger <markfinger@gmail.com>
2015-01-26 19:42:45 +1100

    Removing the tests from the PyPI package data.

--- setup.py
+++ setup.py
@@ -6,12 +6,6 @@ setup(
     name='django-node',
     version=VERSION,
     packages=['django_node'],
-    package_data={
-        'django_node': [
-            'tests/*.py',
-            'tests/package.json',
-        ]
-    },
     install_requires=['django'],
     description='Bindings and utils for integrating Node.js and NPM into a Django application',
     long_description='Documentation at https://github.com/markfinger/django-node',
